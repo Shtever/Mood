1c3dcfe14049f0b9f4b1e3ea0b347f98abcc1363
Stephen McMillen <Shtevetm@gmail.com>
2020-02-24 14:11:00 -0600

    Re-Enter button is somewhat functional. It resets BOTH SO & PO divs.

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ $(".checkbox").change(function () {
 });
 
 $("#submitSO").click(function () {
-    event.preventDefault();
     $("#salesOrder").html("<h2>SO#: " + ($("#SO")[0].value) + "</h2>");
     console.log("SO #:" + $("#SO")[0].value);
     $(".SOForm").hide();
-    $("#reSO").html('<input type="submit" id="SOReEnter" value="Re-Enter"></button>');
+    $("#reSO").html('<form><input type="submit" id="SOReEnter" value="Re-Enter"></form>');
+    event.preventDefault();
 })
 
 $("#submitPO").click(function () {
-    event.preventDefault();
     $("#purchaseOrder").html("<h2>PO#: " + ($("#PO")[0].value) + "</h2>");
     console.log("PO #:" + $("#PO")[0].value);
     $(".POForm").hide();
-    $("#rePO").html('<input type="submit" id="POReEnter" value="Re-Enter"></button>');
+    $("#rePO").html('<form><input type="submit" id="POReEnter" value="Re-Enter"></form>');
+    event.preventDefault();
 })
 
-$("#SOReEnter").click(function(){
-console.log("SO Re-Enter button pressed");
+$("#SOReEnter").click(function () {
+    console.log("SO Re-Enter button pressed");
+    event.preventDefault();
+    $(".SOForm").show();
 })

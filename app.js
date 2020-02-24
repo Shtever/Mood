a662449983ede05c211e7a5702ccbb46ffc3604e
Stephen McMillen <Shtevetm@gmail.com>
2020-02-24 13:16:35 -0600

    SO & PO numbers render on page. Added RE-ENTER button in case of user typos. These RE-ENTER buttons are not functional yet.

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-$(".checkbox").change(function() {
-    if(this.checked) {
+$(".checkbox").change(function () {
+    if (this.checked) {
         console.log($(this).attr("id") + " checked")
     }
+    event.preventDefault();
 });
+
+$("#submitSO").click(function () {
+    event.preventDefault();
+    $("#salesOrder").html("<h2>SO#: " + ($("#SO")[0].value) + "</h2>");
+    console.log("SO #:" + $("#SO")[0].value);
+    $(".SOForm").hide();
+    $("#reSO").html('<input type="submit" id="SOReEnter" value="Re-Enter"></button>');
+})
+
+$("#submitPO").click(function () {
+    event.preventDefault();
+    $("#purchaseOrder").html("<h2>PO#: " + ($("#PO")[0].value) + "</h2>");
+    console.log("PO #:" + $("#PO")[0].value);
+    $(".POForm").hide();
+    $("#rePO").html('<input type="submit" id="POReEnter" value="Re-Enter"></button>');
+})
+
+$("#SOReEnter").click(function(){
+console.log("SO Re-Enter button pressed");
+})

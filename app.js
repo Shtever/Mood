4ab8a8231c710606ce5ef9401617aa37ab3165d0
Stephen McMillen <Shtevetm@gmail.com>
2020-03-04 13:50:06 -0600

    revised SO & PO reset buttons. They no longer refresh the page, and each field can be reset without affecting the other.

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ allCheckboxes();
 // id = new ID given to 'sub' checkboxes
 // lbl = label of 'sub' checkboxes (text next to checkbox)
 
-function checks(boxID, sub, id, lbl){
+function checks(boxID, sub, id, lbl) {
     $(boxID).change(function () {
         if (this.checked) {
             console.log($(this).attr("id") + " checked");
-            $(sub).append('<input type="checkbox" class="checkboxSub" id="'+ id + '">');
+            $(sub).append('<input type="checkbox" class="checkboxSub" id="' + id + '">');
             $(sub).append('<label>' + lbl + '</label>');
         }
         else if (!this.checked) {
@@ -22,10 +22,10 @@ function checks(boxID, sub, id, lbl){
         }
         event.preventDefault();
     });
-} 
+};
 
 // ******* Run ALL checkbox functions ******* //
-function allCheckboxes(){
+function allCheckboxes() {
     checks("#preWire", "#PWcheck", "PW", "Sub-Prewire task")
     checks("#soundInstall", "#SIcheck", "SI", "Sound sub task")
     checks("#videoInstall", "#VIcheck", "VI", "Video sub task")
@@ -35,26 +35,35 @@ function allCheckboxes(){
     checks("#other3", "#other3check", "oth3", "item3 sub task")
     checks("#other4", "#other4check", "oth4", "item4 sub task")
     checks("#other5", "#other5check", "oth5", "item5 sub task")
-}
+};
 
 $("#submitSO").click(function () {
     event.preventDefault();
     $("#salesOrder").html("<h2>SO#: " + ($("#SO")[0].value) + "</h2>");
     console.log("SO #:" + $("#SO")[0].value);
     $(".SOForm").hide();
-    $("#reSO").html('<form><input type="submit" id="SOReEnter" value="Re-Enter"></form>');
-})
+    $("#reSO").html('<button id="SOReEnter" type="button" onclick="SOReEnter()">Re-Enter</button>');
+});
 
 $("#submitPO").click(function () {
     event.preventDefault();
     $("#purchaseOrder").html("<h2>PO#: " + ($("#PO")[0].value) + "</h2>");
     console.log("PO #:" + $("#PO")[0].value);
     $(".POForm").hide();
-    $("#rePO").html('<form><input type="submit" id="POReEnter" value="Re-Enter"></form>');
-})
+    $("#rePO").html('<button id="POReEnter" type="button" onclick="POReEnter()">Re-Enter</button>');
+});
 
-$("#SOReEnter").click(function () {
-    console.log("SO Re-Enter button pressed");
+function SOReEnter() {
     event.preventDefault();
-    $(".SOForm").show();
-})
+    console.log("SO Re-Enter button pressed");
+    $("#salesOrder").html('')
+    $("#reSO").html('<p class="SOForm">SO #:<input type="text" class="input SOForm" id="SO" name="SO"><input type="submit" class="input SOForm" id="submitSO" value="Save"></p>');
+};
+
+function POReEnter() {
+    event.preventDefault();
+    console.log("PO Re-Enter button pressed");
+    $("#purchaseOrder").html('')
+    $("#rePO").html('<p class="POForm">PO #:<input type="text" class="input POForm" id="PO" name="PO"><input type="submit" class="input POForm" id="submitPO" value="Save"></p>');
+
+};
